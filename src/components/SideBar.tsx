ab282d43f458aca160fa0d9723e0803b14a3f4a3
Luiz Fernando Cardozo <luizfcardozo@gmail.com>
2021-09-19 12:20:44 -0300

    initial componentization

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  id: number
+  genres: GenreResponseProps[]
+  handleClickButton: (id: number) => void
+} 
+
+export function SideBar({id, genres, handleClickButton}: SideBarProps) {
+
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={id === genre.id}
+          />
+        ))}
+      </div>
+  </nav>
+  )
 }
